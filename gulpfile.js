38347fde4b42f8be7470cd3c4692642d661b048f
SAPIENT\adala4 <adalal4@sapient.com>
2017-05-19 18:29:23 +0530

    configured gulpfile for less & added dev-server

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp'),
     babelify = require('babelify'),
     browserify = require('browserify'),
-    source = require('vinyl-source-stream');
+    source = require('vinyl-source-stream'),
+    less = require('gulp-less');
+ 
+gulp.task('less', function () {
+    gulp.src('./src/styles/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('dist/css'));
+});
 
 gulp.task("build", function(){
     browserify('src/main.js')
@@ -16,4 +23,4 @@ gulp.task("copy",function(){
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task("default",["build","copy"]);
+gulp.task("default",["build","sass","less"]);
--- src/main.js
+++ src/main.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import MainComponent from './components/main-component.js';
 
 
-
 ReactDOM.render(<MainComponent />, document.getElementById('app-root'));
